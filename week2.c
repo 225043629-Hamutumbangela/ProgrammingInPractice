d9d5acb32270b83c097daa408634d99df88e21cc
Hero Hamutumbangela <225043629@nust.na>
2026-09-23 13:26:09 +0200

    Add Week 2 C program

---
+++ week2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+         int main() {
+            printf("Municipal Financial Management System\n");
+            printf("welcome to the Municipal Financial Management System\n");
+
+            return 0;
+         }
